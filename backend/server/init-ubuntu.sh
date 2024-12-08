de996f831be217a1b2c98a602f3a70912f378c3d
Fabio Danesin <f.danesin99@gmail.com>
2024-12-08 15:40:25 +0100

    caricamento file statici a startup

--- backend/server/init-ubuntu.sh
+++ backend/server/init-ubuntu.sh
@@ -4,9 +4,18 @@
 #
 
 cd /comfystack/server/
+
 # Installa Go
-apt update && apt install golang ca-certificates -y 
+apt update && apt install golang ca-certificates curl -y 
 # Download delle dipendenze
 go mod download
+
+
+cd static
+# Download dei pacchetti usati per il frontend
+curl -L https://cdn.jsdelivr.net/npm/bulma@latest/css/bulma.min.css > bulma.css # Libreria css
+curl -L https://unpkg.com/htmx.org@latest/dist/htmx.min.js          > htmx.min.js # Libreria HTMX
+cd ..
+
 # Start server
 go run main.go
